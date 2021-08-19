18346146bf9505f56e8a4a7dc1114f8bf3a5e176
TheConqu1stador <43194859+TheConqu1stador@users.noreply.github.com>
2021-08-20 02:42:12 +0300

    init

---
+++ WebApplication1/WebApplication1/Controllers/DialogsController.cs
@@ -0,0 +1,40 @@
+﻿using iMessengerCoreAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("/GetDialogsByClientIDs/")]
+    public class DialogsController : ControllerBase
+    {
+        private static List<RGDialogsClients> rGDialogs = RGDialogsClients.Init();
+
+        [HttpPost]
+        /// <param name="accountId">
+        /// Required account ID.
+        /// </param>
+        public List<Guid> GetDialogsByClientIDs([FromBody] List<Guid> clientGuids)
+        {
+            List<List<Guid>> clientsDialogs = new List<List<Guid>>();
+            foreach (Guid client in clientGuids)
+            {
+                clientsDialogs.Add(rGDialogs.FindAll(x => x.IDClient == client).Select(x => x.IDRGDialog).ToList());
+            }
+
+            if (clientsDialogs.Count == 0) clientsDialogs.Add(new List<Guid>());
+
+            var intersection = clientsDialogs.Aggregate((previousList, nextList) => previousList.Intersect(nextList).ToList());
+
+            if (intersection.Count == 0) intersection.Add(Guid.Empty);
+
+            return intersection;
+        }
+    }
+}
